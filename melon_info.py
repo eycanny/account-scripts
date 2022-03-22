cee2b0853b99975c8369d78f898d3ab271eabdf9
eycanny <nancy.the.coder@gmail.com>
2022-03-21 19:00:13 -0700

    Removed old code

--- melon_info.py
+++ melon_info.py
@@ -11,12 +11,3 @@ def print_all_melons(melons):
         for attribute, value in melons[melon].items():
             print(str(attribute) + ": " + str(value))
         print()
-#     have_or_have_not = 'have'
-#     if seedless:
-#         have_or_have_not = 'do not have'
-
-#     print(f'{name}s {have_or_have_not} seeds and are ${price}')
-
-
-# for i in melon_names:
-#     print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
