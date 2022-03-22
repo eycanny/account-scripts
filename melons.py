0086b0ac5a60efd374a66c1722a99ca20eca1914
eycanny <nancy.the.coder@gmail.com>
2022-03-21 18:34:07 -0700

    Combined into one dictionary

--- melons.py
+++ melons.py
@@ -1,35 +1,37 @@
 melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
-
-melon_flesh_color = {
-
-}
-
-melon_rind_color = {
-
-}
-
-melon_avg_weight = {
-    
+    'Honeydew' : {
+        'price' : 0.99,
+        'seedlessness' : True,
+        'flesh_color' : None,
+        'rind_color' : None,
+        'avg_weight' : None
+    },
+    'Crenshaw' : {
+        'price' : 2.00,
+        'seedlessness' : False,
+        'flesh_color' : None,
+        'rind_color' : None,
+        'avg_weight' : None
+    },
+    'Crane' : {
+        'price' : 2.50,
+        'seedlessness' : False,
+        'flesh_color' : None,
+        'rind_color' : None,
+        'avg_weight' : None
+    },
+    'Casaba' : {
+        'price' : 2.50,
+        'seedlessness' : False,
+        'flesh_color' : None,
+        'rind_color' : None,
+        'avg_weight' : None
+    },
+    'Cantaloupe' : {
+        'price' : 0.99,
+        'seedlessness' : False,
+        'flesh_color' : None,
+        'rind_color' : None,
+        'avg_weight' : None
+    },
 }
