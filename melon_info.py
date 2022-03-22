5fcb8df57244af3f2792585001e61cdaec5b3eb2
eycanny <nancy.the.coder@gmail.com>
2022-03-21 18:58:24 -0700

    Create print_all_melons()

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,22 @@
 """Print out all the melons in our inventory."""
+import sys
 
+from melons import melons
 
-from melons import melon_names, melon_prices, melon_seedlessness, melon_flesh_color, melon_rind_color, melon_avg_weight 
-
-
-def print_melon(name, price, seedless):
+def print_all_melons(melons):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+    for melon in melons:
+        print(melon.upper())
+        for attribute, value in melons[melon].items():
+            print(str(attribute) + ": " + str(value))
+        print()
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+#     print(f'{name}s {have_or_have_not} seeds and are ${price}')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
